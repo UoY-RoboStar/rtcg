eb6ff73c7e4c976c43ff65d2efeb610013c96235
Matt Windsor <mattwindsor91@gmail.com>
2023-02-28 20:12:33 +0000

    Start adding config model
    This isn't used anywhere yet.

--- internal/gen/config.go
+++ internal/gen/config.go
@@ -1,3 +1,25 @@
 package gen
 
-type Config struct{}
+import "encoding/xml"
+
+// Config contains serialisable configuration for the generator.
+type Config struct {
+	XMLName   xml.Name         `xml:"rtcg-gen"` // XMLName sets the name of the Config struct in XML.
+	Cpps      []CppTarget      `xml:"cpp"`      // Cpps contains CppTarget elements.
+	Makefiles []MakefileTarget `xml:"makefile"` // Makefiles contains MakefileTarget elements.
+}
+
+// CppTarget configures a C++ generator.
+type CppTarget struct {
+	Variant  string    `xml:"variant,attr"` // Variant gives the variant of C++ to generate (e.g. ROS).
+	Includes []Include `xml:"include"`      // Includes contains custom includes.
+}
+
+// MakefileTarget configures a Makefile.
+type MakefileTarget struct{}
+
+// Include captures a custom include header.
+type Include struct {
+	Src      string `xml:"src,attr"`    // Src is the source to include.
+	IsSystem bool   `xml:"system,attr"` // IsSystem, if true, generates a `<>` include.
+}
