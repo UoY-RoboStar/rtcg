062e74da7501ba5329f67b331eab80695a0ee5ea
Pedro Ribeiro <pedro.ribeiro@york.ac.uk>
2023-08-30 04:40:54 -0700

    Adapted to python node's implementation and workaround for issue #16

--- examples/bmon/ros/src/convert/convert.cpp
+++ examples/bmon/ros/src/convert/convert.cpp
@@ -4,18 +4,18 @@
 // BatteryState messages.
 
 #include "convert.h"
-#include "sensor_msgs/BatteryState.h"
+#include "battery_monitor/BatteryState.h"
 #include "battery_monitor/BatteryStatus.h"
 
-unsigned int fromBatteryInfo(const sensor_msgs::BatteryState::ConstPtr& msg)
+unsigned int fromBatteryInfo(const BatteryInfoMsg msg)
 {
   // Model is nat 0-100, ROS is float 0.0-1.0
   return static_cast<unsigned int>(msg->percentage * 100);
 }
 
-sensor_msgs::BatteryState toBatteryInfo(unsigned int value)
+BatteryInfoVal toBatteryInfo(unsigned int value)
 {
-  sensor_msgs::BatteryState msg;
+  BatteryInfoVal msg;
 
   msg.percentage = static_cast<float>(value) / 100.0;
 
@@ -24,16 +24,25 @@ sensor_msgs::BatteryState toBatteryInfo(unsigned int value)
 
 std::string fromBatteryStatus(const BatteryStatusMsg msg)
 {
-  switch (msg->status) {
-    case battery_monitor::BatteryStatus::OK: 
-      return "Ok";
-    case battery_monitor::BatteryStatus::MISSION_CRITICAL: 
-      return "MissionCritical";
-    case battery_monitor::BatteryStatus::SAFETY_CRITICAL:
-      return "SafetyCritical";
-    default:
-      return "Invalid";
+  if (!msg->status.compare("OK")) {
+    return "Ok";
+  } else if (!msg->status.compare("MC")) {
+    return "MissionCritical";
+  } else if (!msg->status.compare("SC")) {
+    return "SafetyCritical";
+  } else {
+    return "Invalid";
   }
+  // switch (msg->status) {
+  //   case battery_monitor::BatteryStatus::OK: 
+  //     return "Ok";
+  //   case battery_monitor::BatteryStatus::MISSION_CRITICAL: 
+  //     return "MissionCritical";
+  //   case battery_monitor::BatteryStatus::SAFETY_CRITICAL:
+  //     return "SafetyCritical";
+  //   default:
+  //     return "Invalid";
+  // }
   //return std::string(msg->data.c_str());
 }
 
@@ -42,14 +51,18 @@ BatteryStatusVal toBatteryStatus(const std::string value)
   BatteryStatusVal msg;
   //std_msgs::String msg;
   //msg.data = value.c_str();
-  msg.status = BatteryStatusVal::UNSET;
+  //msg.status = BatteryStatusVal::UNSET;
+  msg.status = "UNSET";
 
   if (value.compare("Ok")) {
-    msg.status = BatteryStatusVal::OK;
+    //msg.status = BatteryStatusVal::OK;
+    msg.status = "OK";
   } else if (value.compare("MissionCritical")) {
-    msg.status = BatteryStatusVal::MISSION_CRITICAL;
+    //msg.status = BatteryStatusVal::MISSION_CRITICAL;
+    msg.status = "MC";
   } else if (value.compare("SafetyCritical")) {
-    msg.status = BatteryStatusVal::SAFETY_CRITICAL;
+    //msg.status = BatteryStatusVal::SAFETY_CRITICAL;
+    msg.status = "SC";
   }
 
   return msg;
